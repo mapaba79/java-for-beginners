f5b97bf4c12ce9cc88e814b0c560750728504812
mapaba79 <mateuspaimdebarros23121997@gmail.com>
2025-03-21 13:17:21 -0300

    Variables.java added

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Variables {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        System.out.printf("The sum of %d + %d = %d\n", a, b,  sum);
+
+        scanner.close();
+    }
+}
